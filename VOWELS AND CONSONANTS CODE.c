a5f83a19316b9f4ca42afbbe84857eb7e8c4aa01
arham-chowdary <103562356+arham-chowdary@users.noreply.github.com>
2022-04-15 22:40:36 +0530

    Add files via upload

---
+++ VOWELS AND CONSONANTS CODE.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int
+main ()
+{
+  
+char alphabet;
+  
+int lv, uv;
+  
+printf ("Enter an alphabet: ");
+  
+scanf ("%c", &alphabet);
+  
+ 
+lv = (alphabet == 'a' || alphabet == 'e' || alphabet == 'i'
+	   || alphabet == 'o' || alphabet == 'u');
+  
+ 
+ 
+uv = (alphabet == 'A' || alphabet == 'E' || alphabet == 'I'
+	     || alphabet == 'O' || alphabet == 'U');
+  
+ 
+ 
+if (lv || uv)
+    
+printf ("%c - vowel", alphabet);
+  
+  else
+    
+printf ("%c - consonant", alphabet);
+  
+return 0;
+
+
